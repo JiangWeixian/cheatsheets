5db328159eb860aa61f72d8a2e94655f54481fc4
JiangWeixian <saber_wei@163.com>
2020-04-09 21:09:05 +0800

    ✨ sheet detail page

--- pages/sheet/[id].tsx
+++ pages/sheet/[id].tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import { NextPage, NextPageContext } from 'next'
+import { useRouter } from 'next/router'
+import showdown from 'showdown'
+
 import { api, Github } from '~/api'
 
+const Convertor = new showdown.Converter()
+
 const Cheetsheet: NextPage<{ data: Github.Issue[] }> = ({ data }) => {
-  console.log(data)
-  return <div>1</div>
+  const router = useRouter()
+  return (
+    <div className="flex flex-col contianer items-center justify-center bg-gray-100 overflow-scroll">
+      <h3 className="text-6xl text-gray-800">{router.query.id}</h3>
+      <div className="w-3/4">
+        {data?.map(v => {
+          return (
+            <div className="w-2/4 pr-4 pb-4" style={{ float: 'left' }}>
+              <p className="text-blue-600 mb-4">{v.title}</p>
+              <div
+                className="shadow p-4 w-full rounded overflow-hidden"
+                key={v.title}
+                dangerouslySetInnerHTML={{ __html: Convertor.makeHtml(v.body || '') }}
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 Cheetsheet.getInitialProps = async (ctx: NextPageContext) => {
