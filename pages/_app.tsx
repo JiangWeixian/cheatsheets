d249bfbd82c2239790a817067eeaee95a8f78c74
jiangweixian <jiangweixian1994@gmail.com>
2020-04-11 13:17:09 +0800

    🔥 (app): rematch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,11 @@ import App from 'next/app'
 import React from 'react'
 import '~/style/tailwind.css'
 
-import { withRematch } from '~/utils/rematch'
-
 class CustomApp extends App {
   render() {
     const { Component, pageProps } = this.props
-    return (
-      <Component {...pageProps} />
-    )
+    return <Component {...pageProps} />
   }
 }
 
-export default withRematch(CustomApp)
+export default CustomApp
