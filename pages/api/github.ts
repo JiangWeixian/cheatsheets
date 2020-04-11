13becdad60b2ae67b05bc282705204a5251a11ea
jiangweixian <jiangweixian1994@gmail.com>
2020-04-11 13:14:25 +0800

    🔥 (api): remove user

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -3,7 +3,7 @@ import { api } from '~/api'
 
 export default async (_: NextApiRequest, res: NextApiResponse) => {
   try {
-    const items = await api.users.list()
+    const items = await api.github.labels()
     res.status(200).json(items)
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message })
