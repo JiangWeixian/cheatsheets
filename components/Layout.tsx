52d279a9959c4508b4a4d5d9eacce125e702f88e
jiangweixian <jiangweixian1994@gmail.com>
2020-04-11 15:43:23 +0800

    💄 update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -28,7 +28,7 @@ const Layout = ({ children }: Props) => (
           onClick={() => {
             window.open(`https://github.com/${pkg.author.name}`)
           }}
-          className="mr-4 fill-current text-gray-500 hover:text-gray-700 cursor-pointer"
+          className="mr-8 fill-current text-gray-500 hover:text-gray-700 cursor-pointer"
         />
         <T
           width={20}
