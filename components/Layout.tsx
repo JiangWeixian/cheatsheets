0cf0823bda38b7193a466b3be9c963229c4dfd90
jiangweixian <jiangweixian1994@gmail.com>
2020-04-11 14:24:36 +0800

    ✨ goback

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ArrowLeft } from 'styled-cssgg'
+import router from 'next/router'
 
 type Props = {
   children?: React.ReactNode
@@ -8,7 +9,12 @@ type Props = {
 const Layout = ({ children }: Props) => (
   <div className="w-full h-full">
     <div className="fixed flex w-full h-10 flex-start items-center px-4">
-      <ArrowLeft className="w-10 h-10 text-gray-500 hover:text-gray-700 cursor-pointer" />
+      <ArrowLeft
+        onClick={() => {
+          router.back()
+        }}
+        className="w-10 h-10 text-gray-500 hover:text-gray-700 cursor-pointer"
+      />
     </div>
     {children}
     <footer>
