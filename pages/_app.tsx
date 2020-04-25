eeb5ce3ca7884b9b8ffd612ddf880bfd6c4b04f9
JiangWeixian <saber_wei@163.com>
2020-04-25 13:59:28 +0800

    ✨ nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import App from 'next/app'
 import React from 'react'
+import Progress from 'nprogress'
+import Router from 'next/router'
+
 import '~/style/tailwind.css'
+import '~/style/nprogress.css'
+
+Router.events.on('routeChangeStart', () => Progress.start())
+Router.events.on('routeChangeComplete', () => Progress.done())
+Router.events.on('routeChangeError', () => Progress.done())
 
 class CustomApp extends App {
   render() {
