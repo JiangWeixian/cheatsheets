d12ae5d1018cef8451f39e5f7be6b30bb9295be6
jiangweixian <jiangweixian1994@gmail.com>
2020-05-03 01:17:17 +0800

    ⚡️ (nav): home nav

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ArrowLeft } from 'styled-cssgg'
+import { Home } from 'styled-cssgg'
 import router from 'next/router'
 import Head from 'next/head'
 
@@ -14,37 +14,42 @@ type Props = {
   children?: React.ReactNode
 }
 
-const Layout = ({ children }: Props) => (
-  <div className="w-full h-full">
-    <Head>
-      <title>jiangweixian's cheatsheet</title>
-    </Head>
-    <div className="fixed flex w-full h-10 items-center px-4 justify-between bg-gray-100 z-50">
-      <ArrowLeft
-        onClick={() => {
-          router.back()
-        }}
-        className="w-10 h-10 text-gray-500 hover:text-gray-700 cursor-pointer"
-      />
-      <div className="flex items-center">
-        <G
-          width={20}
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="w-full h-full">
+      <Head>
+        <title>jiangweixian's cheatsheet</title>
+      </Head>
+      <div className="fixed flex w-full h-10 items-center px-4 justify-between bg-gray-100 z-50">
+        <Home
           onClick={() => {
-            window.open(`https://github.com/${pkg.author.name}`)
+            router.push({
+              pathname: '/',
+            })
           }}
-          className="mr-8 fill-current text-gray-500 hover:text-gray-700 cursor-pointer"
+          style={{ '--ggs': 0.95 } as any}
+          className="w-10 h-10 text-gray-500 hover:text-gray-700 cursor-pointer"
         />
-        <T
-          width={20}
-          onClick={() => {
-            window.open(`https://twitter.com/${pkg.author.name}`)
-          }}
-          className="fill-current text-gray-500 hover:text-gray-700 cursor-pointer"
-        />
+        <div className="flex items-center">
+          <G
+            width={20}
+            onClick={() => {
+              window.open(`https://github.com/${pkg.author.name}`)
+            }}
+            className="mr-8 fill-current text-gray-500 hover:text-gray-700 cursor-pointer"
+          />
+          <T
+            width={20}
+            onClick={() => {
+              window.open(`https://twitter.com/${pkg.author.name}`)
+            }}
+            className="fill-current text-gray-500 hover:text-gray-700 cursor-pointer"
+          />
+        </div>
       </div>
+      {children}
     </div>
-    {children}
-  </div>
-)
+  )
+}
 
 export default Layout
