4201f589d52c3470d36893b957229aba34d2425c
JiangWeixian <jiangwei.xian@bytedance.com>
2020-11-04 20:00:03 +0800

    ✨ - hide scrollbar; - emoji favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,6 +40,10 @@ export default class MyDocument extends Document {
             title="Search CheatSheets"
             type="application/opensearchdescription+xml"
           />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏿‍💻</text></svg>"
+          ></link>
         </Head>
         <body>
           <Main />
