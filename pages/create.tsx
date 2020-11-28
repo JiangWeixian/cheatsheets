96f9ad39a3b16a574073d72b86f3aeededa9d18e
jiangWeixian <jiangweixian1994@gmail.com>
2020-11-28 14:48:01 +0800

    ✨ new cheatsheet by quick path

---
+++ pages/create.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import pkg from '../package.json'
+import { Meta } from '~/components/Meta'
+
+const Create = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.push(`https://www.github.com/${pkg.author}/${pkg.name}/issues/new`)
+  }, [])
+  return <Meta title="creating new issue" description="creating new issue" />
+}
+
+export default Create
