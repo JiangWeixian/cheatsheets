b93bc1a1af0fd325ec63e71b628ddbeefe59ae4d
jiangWeixian <jiangweixian1994@gmail.com>
2021-10-10 12:46:14 +0800

    :fire:  deprecated react-instantsearch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { NextPage, GetServerSideProps } from 'next'
-import { Spinner, Search } from 'styled-cssgg'
+import { Spinner } from 'styled-cssgg'
 import { animated, useTrail } from '@react-spring/web'
 import { QueryStatus } from 'react-query'
 import { api } from '@omcs/request/node'
-import { Typography, Input, Dropdown } from 'granen'
-import { InstantSearch, createConnector, connectHits, Configure } from 'react-instantsearch-dom'
+import { Typography } from 'granen'
 import styled from 'styled-components'
 
 import { Github } from '~/interface/github'
-import { SEARCH_CHEATSHEET_INDEX_NAME, SEARCH_LABELS_INDEX_NAME } from '~/utils/constants'
 import Layout from '~/components/Layout'
 import { Meta } from '~/components/Meta'
 import { Sheet } from '~/components/Sheet'
@@ -89,111 +87,6 @@ const Someday = ({
   )
 }
 
-const unShipProps: any = {
-  enterkeyhint: 'search',
-}
-
-const connectWithQuery = createConnector({
-  displayName: 'WidgetWithQuery',
-  getProvidedProps(props, searchState) {
-    // Since the `attributeForMyQuery` searchState entry isn't
-    // necessarily defined, we need to default its value.
-    const currentRefinement = searchState.attributeForMyQuery || ''
-
-    // Connect the underlying component with the `currentRefinement`
-    return { currentRefinement }
-  },
-  refine(props, searchState, nextRefinement) {
-    // When the underlying component calls its `refine` prop,
-    // we update the searchState with the provided refinement.
-    return {
-      // `searchState` represents the search state of *all* widgets. We need to extend it
-      // instead of replacing it, otherwise other widgets will lose their respective state.
-      ...searchState,
-      attributeForMyQuery: nextRefinement,
-    }
-  },
-  getSearchParameters(searchParameters, props, searchState) {
-    // When the `attributeForMyQuery` state entry changes, we update the query
-    return searchParameters.setQuery(searchState.attributeForMyQuery || '')
-  },
-  cleanUp(props, searchState) {
-    // When the widget is unmounted, we omit the entry `attributeForMyQuery`
-    // from the `searchState`, then on the next request the query will
-    // be empty
-    const { attributeForMyQuery, ...nextSearchState } = searchState
-
-    return nextSearchState
-  },
-})
-
-const SearchBox = ({ currentRefinement, refine }) => (
-  <Input
-    prefix={<Search />}
-    borderless={true}
-    {...unShipProps}
-    type="input"
-    size="lg"
-    value={currentRefinement}
-    onChange={e => refine(e.currentTarget.value)}
-  />
-)
-
-const ConnectedSearchBox = connectWithQuery(SearchBox)
-
-const Item = styled(Dropdown.Item)`
-  && {
-    @apply flex-col items-start;
-  }
-
-  p,
-  h3 {
-    @apply m-0;
-  }
-`
-
-const Hits = connectHits(props => {
-  console.log(props.hits)
-  if (props.hits.length === 0) {
-    return (
-      <Dropdown.Menu>
-        <Item>
-          No Results
-        </Item>
-      </Dropdown.Menu>
-    )
-  }
-  return (
-    <Dropdown.Menu>
-      {props.hits.map(item => {
-        return (
-          <Item key={item.objectID}>
-            <Typography.Title h3={true}>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: item._highlightResult.title?.value || '',
-                }}
-              />
-            </Typography.Title>
-            <Typography.Paragraph>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: item._highlightResult.body?.value || '',
-                }}
-              />
-            </Typography.Paragraph>
-          </Item>
-        )
-      })}
-    </Dropdown.Menu>
-  )
-})
-
-const searchClient = api.getSearchClient(
-  process.env.NEXT_PUBLIC_ALGOLIA_APPID!,
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY!,
-)
-
 const SearchContainer = styled.div`
   @apply mt-48 flex items-center justify-center w-full;
 
@@ -221,25 +114,6 @@ const IndexPage: NextPage<{ recent: Github.Issue[]; someday: Github.Issue[] }> =
     <Layout>
       <Meta />
       <SearchContainer>
-        {/* <InstantSearch
-          indexName={SEARCH_CHEATSHEET_INDEX_NAME}
-          stalledSearchDelay={500}
-          searchClient={searchClient}
-        >
-          <Dropdown
-            trigger="click"
-            getPopupContainer={() => document.querySelector('#SEARCH_CONTAINER')!}
-            content={<Hits />}
-            placement="bottomStart"
-            id="SEARCH_CONTAINER"
-          >
-            <ConnectedSearchBox />
-            <Configure
-              highlightPreTag='<mark class="search-highlight">'
-              highlightPostTag="</mark>"
-            />
-          </Dropdown>
-        </InstantSearch> */}
         <CheatSheetSearchBox />
       </SearchContainer>
       <EventContainer>
